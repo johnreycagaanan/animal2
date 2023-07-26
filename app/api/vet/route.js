f719d52cd796562da6d8b34eb03adddaae775d3b
John Rey <john.cagaanan@supportzebra.com>
2023-07-26 18:46:13 +0800

    Added get all vets and delete all vets endpoint

--- app/api/vet/route.js
+++ app/api/vet/route.js
@@ -36,3 +36,33 @@ export async function POST(request) {
     });
   }
 }
+
+export async function GET(request) {
+  try {
+    const vets = await Vet.find();
+    return NextResponse.json({
+      status: 200,
+      vets,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      message: error.message,
+    });
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    await Vet.deleteMany();
+    return NextResponse.json({
+      status: 200,
+      message: "Deleted successfully",
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      message: error.message,
+    });
+  }
+}
